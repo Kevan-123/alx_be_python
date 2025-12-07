717081da0fd57380527ad5900f3ecc03450f004d
Kevan <denkyira.kevan@gmail.com>
2025-12-07 21:08:41 +0000

    Add ALX-compliant library system with __str__ methods

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,6 +5,9 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
     """EBook class inheriting from Book."""
@@ -12,6 +15,9 @@ class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size  # in KB
+
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
@@ -21,23 +27,21 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count  # number of pages
 
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 
 class Library:
-    """Library class demonstrating composition (has a collection of books)."""
+    """Library class demonstrating composition (has a list of books)."""
 
     def __init__(self):
         self.books = []
 
     def add_book(self, book):
-        """Add any type of Book (Book, EBook, PrintBook) to the library."""
+        """Add a Book, EBook, or PrintBook to the library."""
         self.books.append(book)
 
     def list_books(self):
-        """Prints details of each book."""
+        """Print details of each book."""
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(str(book))
